2886b340199f465b57176b452df1729173206419
Yasantha Niroshan <niroshanyi@gmail.com>
2025-12-23 00:46:15 +0530

    Restructured Absolute Value Filter - Added directory to absolute filter

---
+++ src/Components/absolute_value/tb_absolute_value.vhd
@@ -0,0 +1,82 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity tb_absolute_value is
+end tb_absolute_value;
+
+architecture sim of tb_absolute_value is
+
+    -- DUT signals
+    signal clk   : std_logic := '0';
+    signal rst   : std_logic := '1';
+    signal valid_in  : std_logic := '0';
+    signal x_in  : signed(15 downto 0) := (others => '0');
+    signal valid_out : std_logic := '0';
+    signal y_out : signed(15 downto 0);
+
+    constant CLK_PERIOD : time := 10 ns;
+    constant Q16_15 : integer := 32767; -- 2^15
+
+begin
+    -- clock generation
+    clk_process : process
+    begin
+        while true loop
+            clk <= '0';
+            wait for CLK_PERIOD/2;
+            clk <= '1';
+            wait for CLK_PERIOD/2;
+        end loop;
+    end process;
+
+    -- instantiate DUT
+    DUT: entity work.absolute_value
+        port map (
+            clk   => clk,
+            rst   => rst,
+            valid_in  => valid_in,
+            x_in  => x_in,
+            valid_out => valid_out,
+            y_out => y_out
+        );
+
+    -- stimulus
+    stim_proc: process
+    begin
+        -- reset
+        rst <= '1';
+        valid_in <= '0';
+        wait for 2*CLK_PERIOD;
+        rst <= '0';
+        wait for CLK_PERIOD;
+
+        
+        -- Enable valid_in to process data
+        valid_in <= '1';
+
+        -- Test positive value
+        x_in <= to_signed(0, 16);
+        wait for CLK_PERIOD;
+        -- Test negative value
+        x_in <= to_signed(-1 * Q16_15, 16);
+        wait for CLK_PERIOD;
+        -- Test zero
+        x_in <= to_signed(0, 16);
+        wait for CLK_PERIOD;
+        -- Test max positive
+        x_in <= to_signed(1 * Q16_15, 16);
+        wait for CLK_PERIOD;
+        -- Test max negative
+        x_in <= to_signed(-1 * Q16_15, 16);
+        wait for CLK_PERIOD;
+        -- Test random negative
+        x_in <= to_signed(-1 * Q16_15, 16);
+        wait for CLK_PERIOD;
+        -- Test random positive
+        x_in <= to_signed(1 * Q16_15, 16);
+        wait for CLK_PERIOD;
+
+        wait;
+    end process;
+end sim;
