1241774fc5156e04ab417069ca2c811e1f48a1db
Yasantha Niroshan <niroshanyi@gmail.com>
2025-12-23 03:59:09 +0530

    Added Makefiles to modularize the building process - Added makefile sources - Added makefile with minimum configs

---
+++ src/Components/absolute_value/sources.mk
@@ -0,0 +1,2 @@
+VHDL_SYNTH_SOURCES += $(COMPONENT_DIR)/absolute_value/absolute_value.vhd
+VHDL_TB_SOURCES += $(COMPONENT_DIR)/absolute_value/tb_absolute_value.vhd
---
+++ src/Components/peak_detector/sources.mk
@@ -0,0 +1,2 @@
+VHDL_SYNTH_SOURCES += $(COMPONENT_DIR)/peak_detector/peak_detector.vhd
+VHDL_TB_SOURCES += $(COMPONENT_DIR)/peak_detector/tb_peak_detector.vhd
